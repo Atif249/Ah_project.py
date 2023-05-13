a710fd7be649b7fa4c8ebff7d5f87e4b3f873967
Atif-Xiddi <56693128+Atif249@users.noreply.github.com>
2023-05-13 05:05:53 -0700

    Add files via upload

---
+++ AH_Atif.py
@@ -0,0 +1,39 @@
+'''
+ASALAMUALAKUM
+
+AH / ATIF ULLAH
+
+AS GAME KO MANA BNAYA SA JUST BIGGENER KALIYA YA MA NA MOBILE MA HE BANAYA HA OUR AP LOG ASA DOWNLOAD KARKA AS SOURCE CODE PERPRACTICE KAR SAKTA HA SHUKRIA
+
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+owner [ ATIF ULLAH ]
+language ( python )
+date : wed 10 may
+time : 9 : 54 pm
+
+°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+
+
+
+
+'''
+
+counter = 3
+while True:
+	print("2 + 2 - 2 x 2 ")
+	user=input("ENTER YOUR ANS : ")
+	if user == '4' :
+		print("CONGRAGULATION CORRECT ANSWER :" , user)
+		break
+	else:
+		counter = counter-1
+		print("WRONG TRY AGAIN ONLY ",counter," GUESE " )
+		if counter<=0:
+			print("SORRY YOU LOSE THIS GAME")
+		
+			break
+			
+		
+		
+	
+	
